1f93f4a12380c38a1a9729de3fa1a3d30ab0e96a
Hinderchik <gondonloxlp@gmail.com>
2026-04-22 16:25:33 +0300

    termux-tools: add custom XunKal1 Terminal motd

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -13,12 +13,7 @@ TERMUX_PKG_BREAKS="termux-keyring (<< 1.9)"
 TERMUX_PKG_CONFLICTS="procps (<< 3.3.15-2)"
 TERMUX_PKG_SUGGESTS="termux-api"
 
-# Some of these packages are not dependencies and used only to ensure
-# that core packages are installed after upgrading (we removed busybox
-# from essentials).
 TERMUX_PKG_DEPENDS="bzip2, coreutils, curl, dash, diffutils, findutils, gawk, grep, gzip, less, procps, psmisc, sed, tar, termux-am (>= 0.8.0), termux-am-socket (>= 1.5.0), termux-core, termux-exec, util-linux, xz-utils, dialog"
-
-# Optional packages that are distributed as part of bootstrap archives.
 TERMUX_PKG_RECOMMENDS="ed, dos2unix, inetutils, net-tools, patch, unzip"
 
 termux_step_pre_configure() {
@@ -33,4 +28,53 @@ termux_step_create_debscripts() {
 	cat <<- EOF > ./preinst
 	$(cat "$TERMUX_PKG_BUILDDIR/preinst")
 	EOF
+}
+
+termux_step_post_massage() {
+    # Вызываем стандартную обработку
+    termux_step_post_massage
+    
+    # Заменяем com.termux на новый package name во всех файлах
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./bin/termux-fix-shebang
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./bin/termux-info
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./bin/termux-open
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./bin/termux-open-url
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./libexec/termux-api
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./libexec/termux-api-client
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./libexec/termux-am
+    sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" ./libexec/termux-commands
+    
+    # Рекурсивно во всех остальных файлах
+    find . -type f -exec sed -i "s/com.termux/${TERMUX_APP_PACKAGE}/g" {} \;
+    
+    # ===== ДОБАВЛЯЕМ КАСТОМНЫЙ MOTD =====
+    # Отключаем оригинальный скрипт вывода motd
+    chmod -x ./libexec/termux-messages 2>/dev/null || true
+    
+    # Создаём свой motd
+    cat > ./etc/motd << 'EOF'
+Welcome to Termux!
+
+Developer Fork: https://github.com/Hinderchik
+Fork: https://github.com/Hinderchik/XunKal1-termux
+Docs:       https://termux.dev/docs
+Donate:     https://termux.dev/donate
+Community:  https://termux.dev/community
+
+Working with packages:
+
+ - Search:  pkg search <query>
+ - Install: pkg install <package>
+ - Upgrade: pkg upgrade
+
+Subscribing to additional repositories:
+
+ - Root:    pkg install root-repo
+ - X11:     pkg install x11-repo
+
+For fixing any repository issues,
+try 'termux-change-repo' command.
+
+Report issues at https://termux.dev/issues
+EOF
 }
